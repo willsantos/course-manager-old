9eacfa3cb4f8782c23272e0d7cce871110e55102
Wilson Santos <contato@wilsonsantos.net>
2022-06-17 19:35:44 -0300

    ✨ feat: Add course-list-component
    Eu importei o que já tinha feito usando a versão mais atual, mas
    regredindo para acompanhar com a mesma versao do curso.
    
    Dessa forma já foi criado o template html, o component, o serviço e a
    model.

---
+++ src/app/courses/course-list.componet.ts
@@ -0,0 +1,33 @@
+import { CourseService } from "./couse.service";
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+
+@Component({
+  selector: "app-course-list",
+  templateUrl: "./course-list.component.html",
+})
+export class CourseListComponent implements OnInit {
+  _courses: Course[] = [];
+  _filterBy: string = "";
+  filteredCourses: Course[] = [];
+
+  constructor(private courseService: CourseService) {}
+
+  ngOnInit(): void {
+    this._courses = this.courseService.retrieveAll();
+    this.filteredCourses = this._courses;
+  }
+
+  set filter(value: string) {
+    this._filterBy = value;
+
+    this.filteredCourses = this._courses.filter(
+      (course: Course) =>
+        course.name.toLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1
+    );
+  }
+
+  get filter() {
+    return this._filterBy;
+  }
+}
