82697a7c880753b333b1882f505f4fbdd6a22ff2
Wilson Santos <contato@wilsonsantos.net>
2022-07-14 09:33:39 -0300

    ✨ Segregates nav bar into a core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
+import { CoreModule } from "./core/core.module";
 import { NotFoundComponent } from "./not-found/not-found.component";
-import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { NavBarComponent } from "./core/component/nav-bar/nav-bar.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
@@ -8,11 +9,12 @@ import { HttpClientModule } from "@angular/common/http";
 import { CourseModule } from "./courses/ course.module";
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, NotFoundComponent],
+  declarations: [AppComponent, NotFoundComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       { path: "", redirectTo: "courses", pathMatch: "full" },
       { path: "**", component: NotFoundComponent },
